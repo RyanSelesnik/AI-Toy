1e0fbf1711eed38a6ff065f143ee345cae16ff4d
Ryan Selesnik <selesnikryan@gmail.com>
2022-10-03 14:35:06 +0200

    dsv1

---
+++ src/dSv1/ONNXOptimisations.py
@@ -0,0 +1,14 @@
+import argparse
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers.convert_graph_to_onnx as onnx_convert
+
+
+def main():
+    model_name = 'microsoft/DialoGPT-medium'
+
+    pass
+
+
+if __name__ == '__main':
+    main()
