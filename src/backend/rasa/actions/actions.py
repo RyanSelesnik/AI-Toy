d14565b2bc2a33ed50c4ec515e9f49037b2757b1
Ryan Selesnik <selesnikryan@gmail.com>
2022-10-18 14:20:00 +0200

    NLU pipeline now uses RegexEntityExtractor to extract math game numbers

--- src/backend/rasa/actions/actions.py
+++ src/backend/rasa/actions/actions.py
@@ -3,14 +3,17 @@ from rasa_sdk import Action, Tracker
 from typing import Any, Text, Dict, List
 
 
-class ActionHelloWorld(Action):
+class MathGameCount(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_math_game_count"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        test = tracker.get_latest_entity_values()
+        print(test)
 
         dispatcher.utter_message(text="Hello World!")
 
